11c89f9259f47b0339f3f2c372a6e33eeab9d616
Drazen Plavsic <drazen.p@live.com>
2025-05-27 21:05:58 +0200

    adjusted write table, added eventlistener for select opions, adjusted fetch for new api end point

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-const BZN = [
-    "AT", "BE", "CH", "CZ", "DE-LU", "DE-AT-LU",
-    "DK1", "DK2", "FR", "HU", "IT-North", "NL",
-    "NO2", "PL", "SE4", "SI"
-  ];
+window.addEventListener('DOMContentLoaded', () => {
+    const bznSelect = document.getElementById('country');
+    const bzn = [
+        "AT", "BE", "CH", "CZ", "DE-LU", "DE-AT-LU",
+        "DK1", "DK2", "FR", "HU", "IT-North", "NL",
+        "NO2", "PL", "SE4", "SI"
+    ];
 
-function printHeader() {
-    let html = "<thead>";
-    html += "<tr>";
-    html += "<th scope='col'>Zeit</th>";
-    html += "<th scope='col'>Preis</th>";
-    html += "<th scope='col'>Einheit</th>";
-    html += "</tr>";
-    html += "</thead >";
-    return html;
-}
-function printRow(priceObj) {
-    let html = "<tr>";
-    html += "<td>" +
-        new Date(priceObj.start_timestamp).toLocaleTimeString()
-        + "</td>";
-    html += "<td>" + priceObj.marketprice + "</td>";
-    html += "<td>" + priceObj.unit + "</td>";
-    html += "</tr >";
-    return html;
-}
+    bzn.forEach(bzn => {
+        const option = document.createElement('option');
+        option.value = bzn;
+        option.textContent = bzn;
+        bznSelect.appendChild(option);
+    });
+
+    bznSelect.value = 'AT'; // Set default
+});
+
 function setContent(id) {
     let contents = document.querySelectorAll(".content");
     let links = document.querySelectorAll(".nav-link");
@@ -36,53 +28,84 @@ function setContent(id) {
 
 
     if (id == "fetch") {
-        let url = "https://api.energy-charts.info/price?";
-        url += "start=" + $("#from").val(); //$("#[name]") -> id
-        url += "&end=" + $("#to").val();
+        const country = $("#country").val();
+        const start = $("#from").val();
+        const end = $("#to").val();
+
+        let url = "http://localhost:3000/api/price?";
+        url += "bzn=" + country;
+        url += "&start=" + start;
+        url += "&end=" + end;
+
         $.get(url).then((resp) => {
+            let prices = {};
+            prices = resp;
+            myData.series = [];
 
-            let prices = resp.data;
-            console.log(myData);
-            myData["series"] = []; // erzeugt einen neue property
-            console.log(myData);
-            let chartline = {};
-            chartline["name"] = "EPEX-Spot-Preice"; // erzeugt eine NEUSES property name in chartline
-            let dataPoints = [];
-            chartline["data"] = dataPoints;
-            chartline.fillOpacity = 0.1;
+            let chartline = {
+                name: "Day-ahead spot market price ",
+                data: [],
+                fillOpacity: 0.1
+            };
 
-            prices.forEach((price) => {
-                // p[0] = price.start_timestamp; // [x-Wert] Zeitachse
-                // p[1] = price.marketprice; // [y-Wert] Preis
-                dataPoints.push([price.start_timestamp, price.marketprice]);
+            for (let i = 0; i < prices.unix_seconds.length; i++) {
+                let timestamp = prices.unix_seconds[i] * 1000; // Convert to milliseconds
+                let marketprice = prices.price[i];
+                chartline.data.push([timestamp, marketprice]);
             }
-            );
+
             myData.series.push(chartline);
+
+            // Render chart
             Highcharts.chart("mychart", myData);
-            writeTable();
+            writeTable(country, start, end);
         });
     }
-    if (id = "map"){getEuropMap()};
+    if (id = "map") { getEuropMap() };
 
 }
 
-function writeTable() {
-    let url = "https://api.awattar.at/v1/marketdata";
+function writeTable(country, start, end) {
+    let url = "http://localhost:3000/api/price?";
+    url += "bzn=" + country;
+    url += "&start=" + start;
+    url += "&end=" + end;
+    $("#mytable tbody").remove();
+    $("#mytable thead").remove();
     $.get(url).then((resp) => {
 
-        let prices = resp.data;
-        console.log(prices);
+        let prices = resp;
         let html = printHeader();
         html += "<tbody>";
-        prices.forEach(price => {
-            html += printRow(price);
-
-        });
+        for (let i = 0; i < resp.price.length; i++) {
+            html += printRow(resp.unix_seconds[i], resp.price[i], resp.unit);
+        }
+        html += "</tbody>";
         html += "</tbody>";
         $("#mytable").append(html);
 
     });
-    console.log("Achtung: dieser Code steht ach dem $.get wird aber vor .then ausgefuert")
+}
+function printHeader() {
+    let html = "<thead>";
+    html += "<tr>";
+    html += "<th scope='col'>Zeit</th>";
+    html += "<th scope='col'>Preis</th>";
+    html += "<th scope='col'>Einheit</th>";
+    html += "</tr>";
+    html += "</thead >";
+    return html;
+}
+function printRow(timeUnix, price, unit) {
+    let date = new Date(timeUnix * 1000);
+    let timeStr = date.toLocaleTimeString();
+
+    let html = "<tr>";
+    html += "<td>" + timeStr + "</td>";
+    html += "<td>" + price + "</td>";
+    html += "<td>" + unit + "</td>";
+    html += "</tr>";
+    return html;
 }
 $(document).ready(() => { // document (dom) ready!
     $("#fetch-link").click(() => { setContent('fetch') });//registering
@@ -102,10 +125,10 @@ let myData = {
         }
     },
     title: {
-        text: 'Strompreise von EPEX-Spot'
+        text: 'Prices'
     },
     subtitle: {
-        text: 'we are now better'
+        text: ''
     },
     xAxis: {
         type: 'datetime' // In ms [epochalzeot]
@@ -187,7 +210,7 @@ const getEuropMap = async () => {
         });
     } catch (error) {
         console.error('Error loading map:', error);
-        document.getElementById('container-map').innerHTML = 
+        document.getElementById('container-map').innerHTML =
             '<div class="alert alert-danger">Error loading map data. Please try again later.</div>';
     }
 };
