3c91f4da293ca2a75b90bf9425a662e420ec336f
Drazen Plavsic <drazen.p@live.com>
2025-05-28 10:59:30 +0200

    adjusted getPieChart, so i can now manipulate the data

--- script.js
+++ script.js
@@ -62,7 +62,37 @@ function setContent(id) {
         });
     }
     if (id = "map") { getEuropMap() };
-    if (id = "other") { getPieChart() };
+    if (id = "other") {
+        // const country = $("#country").val();
+        // const start = $("#from").val();
+        // const end = $("#to").val();
+
+        // let url = "http://localhost:3000//api/power'?";
+        // url += "bzn=" + country;
+        // url += "&start=" + start;
+        // url += "&end=" + end;
+
+        // $.get(url).then((resp) => {
+        //     let prices = {};
+        //     prices = resp;
+        //     myData.series = [];
+
+        //     let chartline = {
+        //         name: "Day-ahead spot market price ",
+        //         data: [],
+        //         fillOpacity: 0.1
+        //     };
+
+        //     for (let i = 0; i < prices.unix_seconds.length; i++) {
+        //         let timestamp = prices.unix_seconds[i] * 1000; // Convert to milliseconds
+        //         let marketprice = prices.price[i];
+        //         chartline.data.push([timestamp, marketprice]);
+        //     }
+
+        //     myData.series.push(chartline);
+        // });
+        getPieChart();
+    };
 
 }
 
@@ -227,99 +257,99 @@ const getEuropMap = async () => {
     }
 };
 
-const getPieChart= async () => {
-    Highcharts.chart('container', {
-        chart: {
-            type: 'pie',
-            custom: {},
-            events: {
-                render() {
-                    const chart = this,
-                        series = chart.series[0];
-                    let customLabel = chart.options.chart.custom.label;
-    
-                    if (!customLabel) {
-                        customLabel = chart.options.chart.custom.label =
-                            chart.renderer.label(
-                                'Total<br/>' +
-                                '<strong>2 877 820</strong>'
-                            )
-                                .css({
-                                    color: '#000',
-                                    textAnchor: 'middle'
-                                })
-                                .add();
-                    }
-    
-                    const x = series.center[0] + chart.plotLeft,
-                        y = series.center[1] + chart.plotTop -
-                        (customLabel.attr('height') / 2);
-    
-                    customLabel.attr({
-                        x,
-                        y
-                    });
-                    // Set font size based on chart diameter
-                    customLabel.css({
-                        fontSize: `${series.center[2] / 12}px`
-                    });
+let myPieChartData = {
+    chart: {
+        type: 'pie',
+        custom: {},
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
+
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>2 877 820</strong>'
+                        )
+                            .css({
+                                color: '#000',
+                                textAnchor: 'middle'
+                            })
+                            .add();
                 }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                        (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
             }
-        },
-        accessibility: {
-            point: {
-                valueSuffix: '%'
-            }
-        },
-        title: {
-            text: '2023 Norway car registrations'
-        },
-        subtitle: {
-            text: 'Source: <a href="https://www.ssb.no/transport-og-reiseliv/faktaside/bil-og-transport">SSB</a>'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                borderRadius: 8,
-                dataLabels: [{
-                    enabled: true,
-                    distance: 20,
-                    format: '{point.name}'
-                }, {
-                    enabled: true,
-                    distance: -15,
-                    format: '{point.percentage:.0f}%',
-                    style: {
-                        fontSize: '0.9em'
-                    }
-                }],
-                showInLegend: true
-            }
-        },
-        series: [{
-            name: 'Registrations',
-            colorByPoint: true,
-            innerSize: '75%',
-            data: [{
-                name: 'EV',
-                y: 23.9
+        }
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    title: {
+        text: '2023 Norway car registrations'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://api.energy-charts.info">Energy charts</a>'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 8,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
             }, {
-                name: 'Hybrids',
-                y: 12.6
-            }, {
-                name: 'Diesel',
-                y: 37.0
-            }, {
-                name: 'Petrol',
-                y: 26.4
-            }]
+                enabled: true,
+                distance: -15,
+                format: '{point.percentage:.0f}%',
+                style: {
+                    fontSize: '0.9em'
+                }
+            }],
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Prouction Typ',
+        colorByPoint: true,
+        innerSize: '60%',
+        data: [{
+            name: 'EV',
+            y: 23.9
+        }, {
+            name: 'Hybrids',
+            y: 12.6
+        }, {
+            name: 'Diesel',
+            y: 37.0
+        }, {
+            name: 'Petrol',
+            y: 26.4
         }]
-    })
+    }]
 }
+
+const getPieChart = async () => { Highcharts.chart('container', myPieChartData) }
